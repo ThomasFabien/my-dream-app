4e1858203e2c4497278159fb60e7a2cd387e5119
Fabien <fabienjmthomas@gmail.com>
2023-06-21 12:29:47 +0200

    end first day

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserRowComponent } from './user-list/user-row/user-row.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserFormComponent,
+    UserListComponent,
+    UserRowComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-list/user-row/user-row.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-row',
+  templateUrl: './user-row.component.html',
+  styleUrls: ['./user-row.component.scss']
+})
+export class UserRowComponent {
+
+}
